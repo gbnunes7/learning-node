76071a3e30d03382e4bd9f5454734a32cda39f87
Gabriel Melo <bielrj170@gmail.com>
2024-07-30 18:29:08 -0300

    basic node.js

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+const string = "oi"
+const number = 3
+const boolean = false
+
+console.log(typeof string, typeof number, typeof boolean)
+
+const firstName = "Gabriel"
+const lastName = " Melo"
+const phrase = firstName + lastName
+const template = `${firstName} ${lastName}`
+
+console.log(phrase,template)
+
+console.log(`${number} é o numero da sorte do`, firstName)
+
+let n = 9
+console.log(n)
+
+n = 10
+
+console.log(n)
+
+const chuva = true
+
+if(chuva) {
+    console.log("guardaChuva")
+} else {
+    console.log("não")
+
+}
