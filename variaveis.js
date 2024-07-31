91ed23f28bcb03c9d441482f5dd35dcc647ccc1d
Gabriel Melo <bielrj170@gmail.com>
2024-07-30 22:37:23 -0300

    learning basic

--- variaveis.js
+++ variaveis.js
@@ -28,3 +28,28 @@ if(chuva) {
     console.log("não")
 
 }
+
+const phras2 = "olá"
+console.log(phras2.toUpperCase())
+
+const n2 = 10
+const n3 = "10"
+
+console.log(String(n2),Number(n3))
+
+function login() {
+    const senha = '123'
+    const login = 'abc'
+
+    const enhainformada = '324'
+    const logininformado = 'acs'
+
+    return(senha === enhainformada && login === logininformado)
+}
+
+console.log(login())
+
+const a = true
+const v = false
+
+console.log((a === true || v === true))
